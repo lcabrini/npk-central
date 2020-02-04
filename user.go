c66342271009093c0a0b3e4d973e776516f502c1
Lorenzo Cabrini <lorenzo.cabrini@gmail.com>
2020-02-04 13:55:43 +0000

    Implemented userList().

--- user.go
+++ user.go
@@ -6,7 +6,37 @@ import (
     "github.com/lcabrini/npk-common"
 )
 
-var userList = `
+func userList() ([]npk.User, error) {
+    sql := `
+    select *
+    from users
+    where status != 'deleted'
+    `
+
+    db, err := npk.DBConnection(config)
+    if err != nil {
+        return nil, err
+    }
+
+    rows, err := db.Query(sql)
+    if err != nil {
+        return nil, err
+    }
+    defer rows.Close()
+
+    var users []npk.User
+    var user npk.User
+    for rows.Next() {
+        err = rows.Scan(&user.Id, &user.Username, &user.Password,
+            &user.CreatedAt, &user.Status)
+        users = append(users, user)
+
+    }
+
+    return users, nil
+}
+
+var userListTpl = `
 {{template "base" .}}
 
 {{define "main"}}
@@ -15,8 +45,13 @@ var userList = `
 `
 
 func ListUsers(w http.ResponseWriter, r *http.Request) {
+    users, err := userList()
+    if err != nil {
+        http.Error(w, err.Error(), http.StatusInternalServerError)
+        return
+    }
     t, _ := template.New("base").Parse(npk.BaseTemplate)
     t.New("navbar").Parse(npk.Navbar)
-    t.New("users").Parse(userList)
-    t.ExecuteTemplate(w, "users", nil)
+    t.New("users").Parse(userListTpl)
+    t.ExecuteTemplate(w, "users", users)
 }
