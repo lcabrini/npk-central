ce4e1717d3ceb9f7d05e6cd0b53a77c1368584f9
Lorenzo Cabrini <lorenzo.cabrini@gmail.com>
2020-02-09 15:27:49 +0000

    Prevent deleting the HQ branch.

--- sql/npk-central_v01.sql
+++ sql/npk-central_v01.sql
@@ -18,3 +18,20 @@ insert into branches(id, name) values(
     '6c2cfb13-82a8-4fc8-85cb-82aedc9b982d',
     'HQ'
 );
+
+drop function if exists delete_branch();
+create function delete_branch() return trigger as $$
+begin
+    if old.id = '6c2cfb13-82a8-4fc8-85cb-82aedc9b982d' then
+        raise exception "cannot delete HQ branch";
+    else
+        return old
+    end if;
+end;
+$$ language plpgsql;
+
+create trigger before_branch_delete
+    before delete on branches
+    for each row
+        execute function delete_branch();
+
